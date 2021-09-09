b638f2b1cdcef12165f25c43ea5215fa18c78d47
mktcode <kontakt@markus-kottlaender.de>
2021-09-09 13:50:33 +0200

    drafting

--- index.js
+++ index.js
@@ -1,48 +1,47 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 const { ethers } = require('ethers')
-const axios = require('axios')
 const { getFirstDeepestValue } = require('@cryptoactions/sdk')
+const axios = require('axios')
+const githubSigner = require('./github-signer.json')
 
 const run = async () => {
   // inputs
-  const signerContractAddress = core.getInput('signer-contract')
-  const rpcNode = core.getInput('rpc-node')
-  const key = core.getInput('key')
   const githubToken = core.getInput('github-token')
   const octokit = github.getOctokit(githubToken)
+  const rpcNode = core.getInput('rpc-node')
+  const walletKey = core.getInput('wallet-key')
+  const contractAddress = core.getInput('contract-address')
 
   // wallet/contract
   const provider = new ethers.providers.JsonRpcProvider(rpcNode)
-  const wallet = new ethers.Wallet(key, provider)
-  const signerContract = new ethers.Contract(signerContractAddress, [], provider)
+  const wallet = new ethers.Wallet(walletKey, provider)
+  const contract = new ethers.Contract(contractAddress, githubSigner.abi, provider)
+  const contractWithWallet = contract.connect(wallet)
 
   // request
-  const requestId = Number(github.context.payload.issue.body)
-  const request = await signerContract.getRequest(requestId)
-  const response = await octokit.graphql(request.query, { nodeId: request.nodeId })
+  console.log(github.context)
+  const request = JSON.parse(github.context.payload.issue.body)
+  const requestDetails = await contractWithWallet.getRequest(request.signatureId)
+  const response = await octokit.graphql(
+    `query($nodeId:ID!) { node(id: $nodeId) { ... on ${requestDetails[0]} } }`,
+    { nodeId: requestDetails[1] }
+  )
   const result = getFirstDeepestValue(response)
   const resultHash = ethers.utils.solidityKeccak256(
     ['string', 'string',  getResultType(result)],
-    [request.query, request.nodeId, result]
+    [requestDetails[0], requestDetails[1], result]
   )
   const signature = await wallet.signMessage(resultHash)
 
-  // send result to destination
-  if (request.destination.startsWith('https://')) {
-    await axios.post(request.destination, { requestId, result, signature })
-  } else {
-    const repository = await octokit.graphql(
-      'query ($nodeId: ID) { node($nodeId) { ... on Repository { name, owner { login } } } }',
-      { nodeId: request.destination }
-    )
-    await octokit.rest.issues.create({
-      repo: repository.node.name,
-      owner: repository.node.owner.login,
-      title: 'Signer Response',
-      body: JSON.stringify({ requestId, result, signature })
-    })
-  }
+  // post result and signature as comment
+  await octokit.rest.issues.createComment({
+    issue_number: github.context.payload.issue.number,
+    repo: github.context.payload.repository.name,
+    owner: github.context.payload.repository.owner,
+    title: 'Signer Response',
+    body: JSON.stringify({ request, result, signature })
+  })
 }
 
 const getResultType = (result) => {
