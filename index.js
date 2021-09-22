2cff559ee43a51201f40512137f402d1d6ffb2c0
mktcode <kontakt@markus-kottlaender.de>
2021-09-22 23:47:07 +0200

    debug

--- index.js
+++ index.js
@@ -3,38 +3,43 @@ const github = require('@actions/github')
 const { ethers } = require('ethers')
 
 const run = async () => {
-  // inputs
-  const githubToken = core.getInput('github-token')
-  const octokit = github.getOctokit(githubToken)
-  const rpcNode = core.getInput('rpc-node')
-  const walletKey = core.getInput('wallet-key')
-
-  // wallet/contract
-  const provider = new ethers.providers.JsonRpcProvider(rpcNode)
-  const wallet = new ethers.Wallet(walletKey, provider)
-
-  // process request
-  const request = JSON.parse(github.context.payload.issue.body)
-  const workflowRunUrl = `https://api.github.com/repos/${request.owner}/${request.repo}/actions/runs/${request.runId}`
-  const workflowRun = await octokit.request(workflowRunUrl)
-  const workflow = await octokit.request(workflowRun.data.workflow_url)
-  const workflowFile = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
-    owner: request.owner,
-    repo: request.repo,
-    path: workflow.data.path,
-    ref: workflowRun.data.head_sha
-  })
-  const message = ethers.utils.arrayify(ethers.utils.solidityKeccak256(
-    ['string', 'uint256'],
-    [workflowFile.data.sha, request.runId]
-  ))
-  const signature = await wallet.signMessage(message)
-  octokit.rest.issues.createComment({
-    issue_number: github.context.issue.number,
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    body: signature
-  })
+  try {
+    // inputs
+    const githubToken = core.getInput('github-token')
+    const octokit = github.getOctokit(githubToken)
+    const rpcNode = core.getInput('rpc-node')
+    const walletKey = core.getInput('wallet-key')
+  
+    // wallet/contract
+    const provider = new ethers.providers.JsonRpcProvider(rpcNode)
+    const wallet = new ethers.Wallet(walletKey, provider)
+  
+    // process request
+    const request = JSON.parse(github.context.payload.issue.body)
+    console.log(request)
+    const workflowRunUrl = `https://api.github.com/repos/${request.owner}/${request.repo}/actions/runs/${request.runId}`
+    const workflowRun = await octokit.request(workflowRunUrl)
+    const workflow = await octokit.request(workflowRun.data.workflow_url)
+    const workflowFile = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+      owner: request.owner,
+      repo: request.repo,
+      path: workflow.data.path,
+      ref: workflowRun.data.head_sha
+    })
+    const message = ethers.utils.arrayify(ethers.utils.solidityKeccak256(
+      ['string', 'uint256'],
+      [workflowFile.data.sha, request.runId]
+    ))
+    const signature = await wallet.signMessage(message)
+    octokit.rest.issues.createComment({
+      issue_number: github.context.issue.number,
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      body: signature
+    })
+  } catch (e) {
+    core.setFailed(e.message)
+  }
 }
 
 run()
